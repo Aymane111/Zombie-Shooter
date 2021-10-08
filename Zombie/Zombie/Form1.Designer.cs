0ad25768d188a5558e2c8d4a4c5b62a62c1c0161
Aymane111 <60976408+Aymane111@users.noreply.github.com>
2021-10-08 13:58:33 +0200

    Add files via upload

---
+++ Zombie/Zombie/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace Zombie
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lab_ammo = new System.Windows.Forms.Label();
+            this.lab_kills = new System.Windows.Forms.Label();
+            this.lab_health = new System.Windows.Forms.Label();
+            this.bar_prog = new System.Windows.Forms.ProgressBar();
+            this.img_joueur = new System.Windows.Forms.PictureBox();
+            this.GameTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.img_joueur)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lab_ammo
+            // 
+            this.lab_ammo.AutoSize = true;
+            this.lab_ammo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lab_ammo.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lab_ammo.Location = new System.Drawing.Point(12, 18);
+            this.lab_ammo.Name = "lab_ammo";
+            this.lab_ammo.Size = new System.Drawing.Size(97, 24);
+            this.lab_ammo.TabIndex = 2;
+            this.lab_ammo.Text = "AMMO: 0";
+            // 
+            // lab_kills
+            // 
+            this.lab_kills.AutoSize = true;
+            this.lab_kills.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lab_kills.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lab_kills.Location = new System.Drawing.Point(376, 18);
+            this.lab_kills.Name = "lab_kills";
+            this.lab_kills.Size = new System.Drawing.Size(86, 24);
+            this.lab_kills.TabIndex = 3;
+            this.lab_kills.Text = "KILLS: 0";
+            // 
+            // lab_health
+            // 
+            this.lab_health.AutoSize = true;
+            this.lab_health.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lab_health.ForeColor = System.Drawing.Color.AliceBlue;
+            this.lab_health.Location = new System.Drawing.Point(687, 18);
+            this.lab_health.Name = "lab_health";
+            this.lab_health.Size = new System.Drawing.Size(98, 24);
+            this.lab_health.TabIndex = 4;
+            this.lab_health.Text = "HEALTH:";
+            this.lab_health.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // bar_prog
+            // 
+            this.bar_prog.BackColor = System.Drawing.SystemColors.Window;
+            this.bar_prog.Location = new System.Drawing.Point(791, 18);
+            this.bar_prog.Name = "bar_prog";
+            this.bar_prog.Size = new System.Drawing.Size(121, 24);
+            this.bar_prog.TabIndex = 5;
+            this.bar_prog.Value = 100;
+            // 
+            // img_joueur
+            // 
+            this.img_joueur.Image = global::Zombie.Properties.Resources.up;
+            this.img_joueur.Location = new System.Drawing.Point(391, 549);
+            this.img_joueur.Name = "img_joueur";
+            this.img_joueur.Size = new System.Drawing.Size(71, 100);
+            this.img_joueur.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.img_joueur.TabIndex = 6;
+            this.img_joueur.TabStop = false;
+            // 
+            // GameTimer
+            // 
+            this.GameTimer.Enabled = true;
+            this.GameTimer.Interval = 20;
+            this.GameTimer.Tick += new System.EventHandler(this.MainTimerEvent);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(924, 661);
+            this.Controls.Add(this.img_joueur);
+            this.Controls.Add(this.bar_prog);
+            this.Controls.Add(this.lab_health);
+            this.Controls.Add(this.lab_kills);
+            this.Controls.Add(this.lab_ammo);
+            this.Name = "Form1";
+            this.Text = "Zombie Shooter";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyIsDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.KeyIsUp);
+            ((System.ComponentModel.ISupportInitialize)(this.img_joueur)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lab_ammo;
+        private System.Windows.Forms.Label lab_kills;
+        private System.Windows.Forms.Label lab_health;
+        private System.Windows.Forms.ProgressBar bar_prog;
+        private System.Windows.Forms.PictureBox img_joueur;
+        private System.Windows.Forms.Timer GameTimer;
+    }
+}
+
---
+++ Zombie/Zombie/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Zombie
+{
+    public partial class Form1 : Form
+    {
+
+        bool goLeft, goRight, goUp, goDown, gameOver;
+        string facing = "up";
+        int playerHealth = 100;
+        int speed = 10;
+        int ammo = 10;
+        int zombieSpeed = 3;
+        Random randNum = new Random();
+        int score;
+        List<PictureBox> zombieLisr = new List<PictureBox>();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MainTimerEvent(object sender, EventArgs e)
+        {
+            if (playerHealth > 0)
+            {
+                bar_prog.Value = playerHealth;
+            }
+            else
+                gameOver = true;
+            lab_ammo.Text = "Ammo: " + ammo;
+            lab_kills.Text = "Kills: " + score;
+            
+            if(goLeft == true && img_joueur.Left > 0) //img_joueur.Left obtient la distance en pixels entre l'extremite gauche du Form et celle de la pictureBox
+            {
+                img_joueur.Left -= speed; // se dirige vers la gauche tant que l 'extremite de la fenetre n'est pas atteinte en avançant de "speed" pîxels
+            }
+            if(goRight == true && img_joueur.Left + img_joueur.Width < this.ClientSize.Width) //cad : joueur se dirige a droite et que la distance entre l'extremite gauche du Form et celle de la pictureBox + la largeur de la pictureBox < la largeur totale du Form (aka extremite droite non atteinte) 
+            {
+                img_joueur.Left += speed; //en se dirigeant vers la droite, la distance entre la picbox et l extremite gauche augmente de "speed" pixels
+            }
+            if(goUp == true && img_joueur.Top > 45)// idem avec le haut du form
+            {
+                img_joueur.Top -= speed;
+            }
+            if (goDown == true && img_joueur.Top + img_joueur.Height < this.ClientSize.Height) 
+            {
+                img_joueur.Top += speed; //idem droite
+            }
+        }
+
+        private void KeyIsDown(object sender, KeyEventArgs e)
+        {
+            if (gameOver) return;
+            if(e.KeyCode == Keys.Left)
+            {
+                goLeft = true;
+                facing = "left";
+                img_joueur.Image = Properties.Resources.left; 
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                goRight = true;
+                facing = "Right";
+                img_joueur.Image = Properties.Resources.right;
+            }
+            if (e.KeyCode == Keys.Up)
+            {
+                
+                facing = "Up";
+                goUp = true;
+                img_joueur.Image = Properties.Resources.up;
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                
+                facing = "Down";
+                goDown = true;
+                img_joueur.Image = Properties.Resources.down;
+            }
+        }
+
+        private void KeyIsUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                goLeft = false;              
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                goRight = false;          
+            }
+            if (e.KeyCode == Keys.Up)
+            {
+                goUp = false;          
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                goDown = false;        
+            }
+            ShootBullet(facing);
+        }
+        private void ShootBullet(string Direction)
+        {
+            Bullet shootBullet = new Bullet();
+            shootBullet.direction = Direction;
+            shootBullet.bulletLeft = img_joueur.Left + (img_joueur.Width / 2); // abscisse apparition de la bullet en pixels = dist avec extremite gauche + demi largeur de l'image du joueur
+            shootBullet.bulletTop = img_joueur.Top + (img_joueur.Height / 2); // ordonnée
+            shootBullet.MakeBullet(this); // une meth de la classe bullet qui necissite un form en params !!
+        }
+        private void MakeZombies()
+        {
+
+        }
+        private void RestartGame()
+        {
+
+        }
+    }
+}
